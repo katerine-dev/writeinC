c249a46e1724e94c3101098ee79536b56f524517
katewitkoski <ka.lwitkoski@gmail.com>
2024-04-11 11:45:55 -0300

    inicio no curso completo

---
+++ AulaSaida/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("Oi, tudo bem? tenho 6 anos e programo. \n");
+
+    printf("Valor Inteiro: %d. \n", 10);
+    printf("Valor real: %f. \n", 3.736363);
+    printf("Valor real com apenas duas casas: %.2f \n", 3.231231);
+    printf("Dado de texto: %c. \n", 'a');
+    printf("Dado de texto: %s. \n", "testando");
+
+}
